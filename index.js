c047500c1b97a94c2795ed217d60f380519b4763
Chi Nguyen <nguyen.thimai@northeastern.edu>
2021-01-14 19:12:37 -0500

    commit tree a469986999e6709d90f41c36d3a8c3aa74797a95

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+
+function findMatching(drivers, string) {
+  return drivers.filter(driver => driver.toLowerCase() === string.toLowerCase());
+};
+
+function fuzzyMatch(drivers, string) {
+  const l = string.length
+  return drivers.filter(driver => driver.slice(0,l).toLowerCase() === string.toLowerCase());
+};
+
+function matchName(drivers, string) {
+  return drivers.filter(driver => driver.name.toLowerCase() === string.toLowerCase());
+
+}
+
